75cea0cc9503b91252d7866ed1298eb658ffd746
Phan Van Khanh <khanhyd2@gmail.com>
2019-07-10 10:34:31 +0700

    Signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,25 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create; end
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      flash[:success] = t ".success"
+      redirect_to root_path
+    else
+      flash[:danger] = t ".fails"
+      render :new
+    end
+  end
 
   private
+
+  def user_params
+    params.require(:user).permit :name, :email, :password, :password_confirmation
+  end
 
   def load_user
     @user = User.find_by id: params[:id]
   end
-
 end
--- db/migrate/20190710021216_create_comic_followers.rb
+++
@@ -1,6 +0,0 @@
-class CreateComicFollowers < ActiveRecord::Migration[5.2]
-  def change
-    create_table :comic_followers do |t|
-    end
-  end
-end
